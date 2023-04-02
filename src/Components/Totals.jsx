cb6ea5100d36e3f5fbe9dd66b47038d385093765
audrbar <audrbar@gmail.com>
2023-04-02 14:42:07 +0300

    background

--- src/Components/Totals.jsx
+++ src/Components/Totals.jsx
@@ -9,7 +9,7 @@ const Totals = () => {
     return (
         <nav className="container mx-auto flex flex-col gap-y-12 items-center justify-between p-4">
             <div className="flex justify-between gap-y-2 gap-x-6">
-                <div className="flex flex-col items-center justify-between back-ground-image gap-y-2 md:gap-x-6">
+                <div className="flex flex-col items-center justify-between gap-y-2 md:gap-x-6">
                     <p className="pt-2 text-xl text-red-600">Clients Total: {list === null ? null : list.length}</p>
                     <p className="pt-2 text-xl text-red-600">Cash Total: {" "}
                         {list?.map((item) => item.amount).reduce((acc, curr) => acc + curr, 0)}{" $"}
@@ -18,7 +18,7 @@ const Totals = () => {
                         {(list?.map((item) => item.amount).reduce((acc, curr) => acc + curr, 0)) / list?.length ?? 0}{" $"}
                     </p>
                 </div>
-                <div className="flex flex-col items-center justify-between back-ground-image gap-y-2 md:gap-x-6">
+                <div className="flex flex-col items-center justify-between gap-y-2 md:gap-x-6">
                     <p className="pt-2 text-xl text-red-600">Debited Accounts: {" "}
                         {list?.filter(acc => acc.amount > 0).length ?? 0}
                     </p>
